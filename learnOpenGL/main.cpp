14565270e739e9d71a77b24c8b383029e06efe60
johnson4500 <johnsonhan6500@gmail.com>
2024-10-14 02:04:48 -0400

    Implement texturing

--- learnOpenGL/main.cpp
+++ learnOpenGL/main.cpp
@@ -1,3 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -10,10 +12,14 @@
 #include <iostream>
 #include <vector>
 
+
 Application App = Application();
 Mesh3D mesh1 = Mesh3D();
+Mesh3D mesh2 = Mesh3D();
 bool isUpPressed = false;
 bool isDownPressed = false;
+unsigned int texture;
+int widthImg, heightImg, numColCh;
 
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
     unsigned int id = glCreateShader(type);
@@ -56,7 +62,7 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 // 
 //}   
 
-void initializeProgram(Application *App) {
+void initializeProgram(Application* App) {
     // Initialize the library
     if (!glfwInit())
         exit(EXIT_FAILURE);
@@ -83,148 +89,189 @@ void initializeProgram(Application *App) {
     fprintf(stdout, "GL version: %s\n", glGetString(GL_VERSION));
 }
 
-void vertexSpecification(Mesh3D *mesh) {
-
+void vertexSpecification(Mesh3D* mesh) {
     // position buffer data
     std::vector<float> positions = {
-   -0.15f, -0.15f,  0.15f, // front bottom left
-    0.15f, -0.15f,  0.15f, // front bottom right
-   -0.15f,  0.15f,  0.15f, // front top left
-    0.15f,  0.15f,  0.15f, // front top right
+        // Top
+         -1.0, 1.0, -1.0,   0, 0,
+         -1.0, 1.0, 1.0,    0, 1,
+         1.0, 1.0, 1.0,     1, 1,
+         1.0, 1.0, -1.0,    1, 0,
 
-   -0.15f, -0.15f, -0.15f, //  back bottom left
-    0.15f, -0.15f, -0.15f, //  back bottom right
-   -0.15f,  0.15f, -0.15f, //  back top left
-    0.15f,  0.15f, -0.15f, //  back top right
-    };
+         // Left
+         -1.0, 1.0, 1.0,    0, 0,
+         -1.0, -1.0, 1.0,   1, 0,
+         -1.0, -1.0, -1.0,  1, 1,
+         -1.0, 1.0, -1.0,   0, 1,
 
-    // color buffer data
-    std::vector<float> colors = {
-        // first triangle
-        1,   0,   0,
-        0,   1,   0,
-        0,   0,   1,
-        // second triangle
-        0,   1,   0,
-        0,   0,   1,
-        0,   0,   1
+         // Right
+         1.0, 1.0, 1.0,    1, 1,
+         1.0, -1.0, 1.0,   0, 1,
+         1.0, -1.0, -1.0,  0, 0,
+         1.0, 1.0, -1.0,   1, 0,
+
+         // Front
+         1.0, 1.0, 1.0,    1, 1,
+         1.0, -1.0, 1.0,    1, 0,
+         -1.0, -1.0, 1.0,    0, 0,
+         -1.0, 1.0, 1.0,    0, 1,
+
+         // Back
+         1.0, 1.0, -1.0,    0, 0,
+         1.0, -1.0, -1.0,    0, 1,
+         -1.0, -1.0, -1.0,    1, 1,
+         -1.0, 1.0, -1.0,    1, 0,
+
+         // Bottom
+         -1.0, -1.0, -1.0,   1, 1,
+         -1.0, -1.0, 1.0,    1, 0,
+         1.0, -1.0, 1.0,     0, 0,
+         1.0, -1.0, -1.0,    0, 1,
     };
 
     // index buffer data
     std::vector<int> indices = {
-        // front face
-        2, 0, 1,
-        3, 2, 1,
-        // left face
-        1, 5, 3,
-        7, 3, 5,
-        // right face
-        5, 7, 4,
-        4, 6, 7,
-        // back face
-        4, 6, 2,
-        2, 0, 4,
-        // top face
-        3, 7, 6,
-        3, 6, 2,
-        // bottom face
-        1, 5, 4,
-        1, 4, 0
+        // Top
+        0, 1, 2,
+        0, 2, 3,
+
+        // Left
+        5, 4, 6,
+        6, 4, 7,
+
+        // Right
+        8, 9, 10,
+        8, 10, 11,
+
+        // Front
+        13, 12, 14,
+        15, 14, 12,
+
+        // Back
+        16, 17, 18,
+        16, 18, 19,
+
+        // Bottom
+        21, 20, 22,
+        22, 20, 23
     };
+
     // create vertex array object
     glGenVertexArrays(1, &(mesh->m_VAO));
     glBindVertexArray(mesh->m_VAO);
 
-    /* 3 step process of creating a buffer, generate->bind->assign data.
-       Below, we create the buffer containing vertices. */
+    // generate->bind->assign data.
     glGenBuffers(1, &(mesh->m_VertexBufferObj));
     glBindBuffer(GL_ARRAY_BUFFER, mesh->m_VertexBufferObj);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * positions.size(), positions.data(), GL_STATIC_DRAW);
 
+    // pos
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 
-    // generate color buffer
-    glGenBuffers(1, &(mesh->m_ColorBufferObj));
-    glBindBuffer(GL_ARRAY_BUFFER, mesh->m_ColorBufferObj);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * colors.size(), colors.data(), GL_STATIC_DRAW);
-
+    // tex coords
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // generate index buffer
     glGenBuffers(1, &(mesh->m_IndexBufferObj));
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->m_IndexBufferObj);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
 
-    //// unbind current buffer
-    //glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // unbind current buffer
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
     // Disable any open attributes
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(2);
+
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    unsigned char* data = stbi_load("sample3b.png", &widthImg, &heightImg, &numColCh, STBI_rgb_alpha);
+    if (!data) {
+        std::cerr << "Failed to load image: " << stbi_failure_reason() << std::endl;
+        exit(EXIT_FAILURE);
+
+    }
+
+    std::cout << "Image loaded successfully: " << widthImg << "x" << heightImg << std::endl;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-void createGraphicsPipeline(Application *App) {
+void createGraphicsPipeline(Application* App) {
     // GLSL vertex and fragment shader source code stored in strings
     std::string vertexShader =
         "#version 330 core\n"
         "\n"
         "layout(location = 0) in vec3 position;\n"
-        "layout(location = 1) in vec3 vertexColors;"
+        //"layout(location = 1) in vec3 vertexColors;\n"
+        "layout(location = 1) in vec2 aTex;\n"
 
-        "uniform mat4 u_ModelMatrix;"
-        "uniform mat4 u_Perspective;"
+        "uniform mat4 u_ModelMatrix;\n"
+        "uniform mat4 u_Perspective;\n"
 
-        "out vec3 v_VertexColors;"
+        //"out vec3 v_VertexColors;\n"
+        "out vec2 texCoord;\n"
 
         "void main()\n"
         "{\n"
-        "   vec4 newPosition = u_Perspective * u_ModelMatrix * vec4(position, 1.0f);"
+        "   vec4 newPosition = u_Perspective * u_ModelMatrix * vec4(position, 1.0f);\n"
         // dont forget w!
         "   gl_Position = vec4(newPosition.x, newPosition.y, newPosition.z, newPosition.w);\n"
-        "   v_VertexColors = vertexColors\n;"
+        //"   v_VertexColors = vertexColors;\n"
+        "   texCoord = aTex;\n"
         "}\n";
 
     std::string fragmentShader =
         "#version 330 core\n"
         "\n"
-        "in vec3 v_VertexColors;\n"
-        "out vec4 fragColor;"
-
+        //"in vec3 v_VertexColors;\n"
+        "in vec2 texCoord;\n"
+        "out vec4 fragColor;\n"
+        "uniform sampler2D tex0;"
         "void main()\n"
         "{\n"
-        "   fragColor = vec4(v_VertexColors, 1.0f);\n"
+        "   fragColor = texture(tex0, texCoord);\n"
         "}\n";
 
     App->m_ShaderProgram = CreateShader(vertexShader, fragmentShader);
-    //glUseProgram(App->m_ShaderProgram);
 }
 
+void meshUpdate(Mesh3D* mesh, Application* app, float dt) {
+    glUseProgram(app->m_ShaderProgram);
 
-
-void meshUpdate(Mesh3D *mesh, Application *app) {
-    // Enable depth test
+    // enable depth test
     glEnable(GL_DEPTH_TEST);
 
-    // Accept fragment if it closer to the camera than the former one
+    // accept fragment if it closer to the camera than the former one
     glDepthFunc(GL_LESS);
 
     glViewport(0, 0, app->m_ScreenWidth, app->m_ScreenHeight);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // std::cout << app->m_ShaderProgram;
-    glUseProgram(app->m_ShaderProgram);
-
+;
     // create model matrix and apply transformations
     glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, mesh->m_uOffset));
     modelMatrix = glm::rotate(modelMatrix, glm::radians(mesh->m_uRotateDegrees), glm::vec3(1.0f, 1.0f, 0.0f));
     modelMatrix = glm::scale(modelMatrix, glm::vec3(mesh->m_uScale, mesh->m_uScale, mesh->m_uScale));
 
     // get location of model matrix
+    
     int modelMatrixLocation = glGetUniformLocation(app->m_ShaderProgram, "u_ModelMatrix");
-
     // error checks
     if (modelMatrixLocation >= 0) {
         //std::cout << modelMatrixLocation << std::endl;
@@ -235,12 +282,8 @@ void meshUpdate(Mesh3D *mesh, Application *app) {
         exit(EXIT_FAILURE);
     }
 
-    /* Projection matrix (perspective)
-    * [ 1/(aspect*tan(fov/2)),      0,                    0,                            0             ]
-    * [          0,            1/tan(fov/2),              0,                            0             ]
-    * [          0,                 0,         -((far+near)/(far-near))    -(*2*far*near)/(far-near)) ]
-    * [          0,                 0,                   -1,                            0             ]
-    */
+    
+    // create perspective matrix
     glm::mat4 perspective = glm::perspective(glm::radians(45.0f),
         (float)app->m_ScreenWidth / (float)app->m_ScreenHeight,
         0.1f,
@@ -248,27 +291,33 @@ void meshUpdate(Mesh3D *mesh, Application *app) {
 
     // get location of perspective matrix
     int projectionLocation = glGetUniformLocation(app->m_ShaderProgram, "u_Perspective");
-
     // error checks
     if (projectionLocation >= 0) {
         //std::cout << projectionLocation << std::endl;
         glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &perspective[0][0]);
-    } else {
+    }
+    else {
         std::cout << "Could not find location of u_Perspective." << std::endl;
         exit(EXIT_FAILURE);
     }
 
     // increase cube rotation with each frame
     if (mesh->m_uRotateDegrees >= 360) mesh->m_uRotateDegrees = 0;
-    mesh->m_uRotateDegrees += 0.05f;
+    mesh->m_uRotateDegrees += 35 * dt;
+
+    int texLocation = glGetUniformLocation(app->m_ShaderProgram, "tex0");
+    glUniform1i(texLocation, 0);
 }
 
-void meshDraw(Mesh3D *mesh, unsigned int pipeline) {
+void meshDraw(Mesh3D* mesh, unsigned int pipeline) {
     if (mesh == nullptr) return;
 
     // per mesh, choose pipeline to be used
     glUseProgram(pipeline);
-    
+
+    // bind texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
     // enable attributes
     glBindVertexArray(mesh->m_VAO);
 
@@ -279,50 +328,47 @@ void meshDraw(Mesh3D *mesh, unsigned int pipeline) {
     glUseProgram(0);
 }
 
-void meshDelete(Mesh3D *mesh) {
+void meshDelete(Mesh3D* mesh) {
     glDeleteBuffers(1, &(mesh->m_VertexBufferObj));
     glDeleteVertexArrays(1, &(mesh->m_VAO));
 }
 
-void keyInput(Application *App) {
+void keyInput(Application* App, float dt) {
     if (glfwGetKey(App->m_Window, GLFW_KEY_UP) == GLFW_PRESS) {
-        mesh1.m_uOffset += 0.025f;
+        mesh1.m_uOffset += 5 * dt;
     }
 
     if (glfwGetKey(App->m_Window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-        mesh1.m_uOffset -= 0.025f;
+        mesh1.m_uOffset -= 5 * dt;
     }
 
     if (glfwGetKey(App->m_Window, GLFW_KEY_E) == GLFW_PRESS) {
-        mesh1.m_uScale += 0.025f;
+        if (mesh1.m_uScale <= 1.0f) mesh1.m_uScale += 2 * dt;
+        else mesh1.m_uScale = 1.0f;
     }
 
     if (glfwGetKey(App->m_Window, GLFW_KEY_Q) == GLFW_PRESS) {
-        mesh1.m_uScale -= 0.025f;
+        if (mesh1.m_uScale >= 0.1f) mesh1.m_uScale -= 2 * dt;
+        else mesh1.m_uScale = 0.1f;
     }
 }
 
-void mainLoop(Application *App) {
+void mainLoop(Application* App) {
     // loop until the user closes the window
+    float previousTime = 0.0;
+    float currentTime = 0.0;
+    float dt = 0.0;
     while (!glfwWindowShouldClose(App->m_Window)) {
-        App->m_LastTime = glfwGetTime();
-        // calculate frames per second
-        /*App->m_LastTime = App->m_CurrentTime;*/
-        App->m_CurrentTime = glfwGetTime();
-        /*float dt = App->m_CurrentTime - App->m_LastTime;
-        std::cout << dt << std::endl;*/
-        App->m_Frames++;
-        if (App->m_CurrentTime - App->m_LastTime >= 1.0f) {
-            std::cout << App->m_Frames << std::endl;
-            App->m_Frames = 0;
-            App->m_LastTime = App->m_CurrentTime;
-        }
+        // delta time calculations
+        currentTime = glfwGetTime();
+        dt = currentTime - previousTime;
+        previousTime = currentTime;
 
         // input
-        keyInput(App);
-        
+        keyInput(App, dt);
+
         // predraw
-        meshUpdate(&mesh1, App);
+        meshUpdate(&mesh1, App, dt);
 
         // draw call
         meshDraw(&mesh1, App->m_ShaderProgram);
@@ -344,7 +390,10 @@ int cleanUp() {
 
 int main(void) {
     initializeProgram(&App);
-    
+    int width, height, channels;
+
+    stbi_set_flip_vertically_on_load(true);
+
     vertexSpecification(&mesh1);
 
     createGraphicsPipeline(&App);
